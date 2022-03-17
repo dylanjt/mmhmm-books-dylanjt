47536cdace3070fb002809eb2c47d36ff04a5f69
Dylan Taylor <dylan@dt.engineering>
2022-03-17 22:03:37 +0000

    add title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,18 @@
+import Head from 'next/head'
+
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
-    <div className="container mx-auto p-4 lg:px-96 lg:mt-20">
-      <Component {...pageProps} />
-    </div>
+    <>
+      <Head>
+        <title>Bookshelf</title>
+      </Head>
+      <div className="container mx-auto p-4 lg:px-96 lg:mt-20">
+        <Component {...pageProps} />
+      </div>
+    </>
   )
 }
 
