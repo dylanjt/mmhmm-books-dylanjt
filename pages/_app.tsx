a26b7983336094ae59648d45eb83fc52d9c35dc7
Dylan Taylor <dylan@dt.engineering>
2022-03-17 17:41:26 +0000

    preliminary layout work

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,7 +2,11 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 const App = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto md:px-96 md:mt-20">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default App
