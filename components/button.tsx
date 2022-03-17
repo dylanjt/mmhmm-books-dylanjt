6a7472943451640479635a4cd80bbd46bbc68400
Dylan Taylor <dylan@dt.engineering>
2022-03-17 19:56:45 +0000

    cosmetic changes to Button

--- components/button.tsx
+++ components/button.tsx
@@ -10,7 +10,7 @@ interface ButtonProps {
 const Button = ({ children, onClick, loading, disabled }: ButtonProps) => {
   return (
     <button
-      className="bg-green-600 hover:bg-green-500 disabled:bg-green-200 text-white disabled:text-gray-100 disabled:cursor-not-allowed px-4 py-2 rounded"
+      className="bg-green-600 hover:bg-green-500 disabled:bg-green-300 text-white disabled:text-gray-100 disabled:cursor-not-allowed px-4 py-2 rounded"
       onClick={onClick}
       disabled={loading || disabled}
     >
